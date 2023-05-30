13a927884691c6bcd0c7ed4c9162bfe796452704
Patrikgomercic1 <131706932+Patrikgomercic1@users.noreply.github.com>
2023-05-30 08:39:09 +0200

    Stvaranje ER dijagrama
    Stvaranje prve verzije dijagrama  bez spajanja tablica

---
+++ Online_Trgovina.sql
@@ -0,0 +1,39 @@
+﻿use master
+drop database if exists online_trgovina
+go
+create database online_trgovina
+go
+use online_trgovina
+
+
+
+create table kupac(
+	sifra int not null primary key identity(1,1),
+	korisnickoime varchar(30) not null,
+	ime varchar(30) not null,
+	prezime varchar(30) not null,
+	lozinka varchar(50) not null,
+	telefon int not null,
+	adresa varchar(50) not null
+);
+
+create table košarica(
+	sifra int not null primary key identity(1,1),
+	kolicina int not null,
+	kupac int,
+	proizvod int,
+);
+
+create table proizvod(
+	sifra int not null primary key identity(1,1),
+	naziv varchar(20) not null,
+	opis varchar(100) not null,
+	cijena decimal(6,2) not null
+);
+
+create table inventar(
+	sifra int not null primary key identity(1,1),
+	proizvod int,
+	kolicina int not null,
+	dostupnost varchar(13) not null
+);
