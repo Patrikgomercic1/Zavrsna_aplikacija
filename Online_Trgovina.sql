960fbbb8a77b84faec2abff03c33dcded70262c8
Patrikgomercic1 <131706932+Patrikgomercic1@users.noreply.github.com>
2023-06-02 12:51:43 +0200

    Dodavanje vrijednosti
    Dodavanje vrijednosti u tablicu kupac, kasnije dodati i ostale

--- Online_Trgovina.sql
+++ Online_Trgovina.sql
@@ -1,9 +1,9 @@
 ﻿use master
-drop database if exists online_trgovina
+drop database if exists online_trgovina;
 go
-create database online_trgovina
+create database online_trgovina;
 go
-use online_trgovina
+use online_trgovina;
 
 
 
@@ -13,7 +13,7 @@ create table kupac(
 	ime varchar(30) not null,
 	prezime varchar(30) not null,
 	lozinka varchar(50) not null,
-	telefon int not null,
+	telefon varchar(15),
 	adresa varchar(50) not null
 );
 
@@ -43,3 +43,14 @@ create table inventar(
 alter table kosarica add foreign key (kupac) references  kupac(sifra);
 alter table kosarica add foreign key (proizvod) references  proizvod(sifra);
 alter table inventar add foreign key (proizvod) references  proizvod(sifra);
+
+
+
+insert into kupac(sifra,korisnickoime,ime,prezime,lozinka,telefon,adresa)
+values (1,'DarkLord34','Matej','Knežević','Alsmhtj563nb','095 284 2371','Opatijska 12, Osijek');
+
+insert into kupac(sifra,korisnickoime,ime,prezime,lozinka,telefon,adresa)
+values (2,'Avante_marauder2','Eugen','Novak','770#kB7RGsJV','031/150-620','Rapska 37, Zagreb');
+
+insert into kupac(sifra,korisnickoime,ime,prezime,lozinka,telefon,adresa)
+values (3,'OniKyu','Tihana','Babić','1h8h9zYx@Ii@','092 358 1548','Put Ravnih Njiva 30, Split');
