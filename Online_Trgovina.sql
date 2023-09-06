a6395f5b157793376cf85ed2523d3ed824e6a5d6
Patrik Gomerčić <patrik.gomercic3@gmail.com>
2023-09-06 23:36:08 +0200

    Prepravak
    Prepravljanje vrijednosti Sifra

--- Online_Trgovina.sql
+++ Online_Trgovina.sql
@@ -48,14 +48,14 @@ alter table inventar add foreign key (proizvod) references  proizvod(sifra);
 
 --TABLICA KUPAC
 SET IDENTITY_INSERT kupac ON
-insert into kupac(sifra,korisnickoime,ime,prezime,lozinka,telefon,adresa)
-values (1,'DarkLord34','Matej','Knežević','Alsmhtj563nb','095 284 2371','Opatijska 12, Osijek');
+insert into kupac(korisnickoime,ime,prezime,lozinka,telefon,adresa)
+values ('DarkLord34','Matej','Knežević','Alsmhtj563nb','095 284 2371','Opatijska 12, Osijek');
 
-insert into kupac(sifra,korisnickoime,ime,prezime,lozinka,telefon,adresa)
-values (2,'Avante_marauder2','Eugen','Novak','770#kB7RGsJV','031/150-620','Rapska 37, Zagreb');
+insert into kupac(korisnickoime,ime,prezime,lozinka,telefon,adresa)
+values ('Avante_marauder2','Eugen','Novak','770#kB7RGsJV','031/150-620','Rapska 37, Zagreb');
 
-insert into kupac(sifra,korisnickoime,ime,prezime,lozinka,telefon,adresa)
-values (3,'OniKyu','Tihana','Babić','1h8h9zYx@Ii@','092 358 1548','Put Ravnih Njiva 30, Split');
+insert into kupac(korisnickoime,ime,prezime,lozinka,telefon,adresa)
+values ('OniKyu','Tihana','Babić','1h8h9zYx@Ii@','092 358 1548','Put Ravnih Njiva 30, Split');
 
 SET IDENTITY_INSERT kupac OFF
 
@@ -63,14 +63,14 @@ SET IDENTITY_INSERT kupac OFF
 --TABLICA PROIZVOD
 SET IDENTITY_INSERT proizvod ON
 
-insert into proizvod(sifra,naziv,opis,cijena)
-values (1,'NINTENDO Switch Lite-crven','Nintendo Switch Lite je mali i lagan Nintendo Switch sustav po odličnoj cijeni.','259.99');
+insert into proizvod(naziv,opis,cijena)
+values ('NINTENDO Switch Lite-crven','Nintendo Switch Lite je mali i lagan Nintendo Switch sustav po odličnoj cijeni.','259.99');
 
-insert into proizvod(sifra,naziv,opis,cijena)
-values (2,'Battlefield 2042 PS5','Sljedeća generacija sveobuhvatnog rata je ovdje.','79.99');
+insert into proizvod(naziv,opis,cijena)
+values ('Battlefield 2042 PS5','Sljedeća generacija sveobuhvatnog rata je ovdje.','79.99');
 
-insert into proizvod(sifra,naziv,opis,cijena)
-values (3,'LED fleksibilna traka','Proširite mogućnosti osvjetljenja vašeg doma. 1m, 2.1W, 24V','11.59');
+insert into proizvod(naziv,opis,cijena)
+values ('LED fleksibilna traka','Proširite mogućnosti osvjetljenja vašeg doma. 1m, 2.1W, 24V','11.59');
 
 SET IDENTITY_INSERT proizvod OFF
 
@@ -78,14 +78,14 @@ SET IDENTITY_INSERT proizvod OFF
 --TABLICA INVENTAR
 SET IDENTITY_INSERT inventar ON
 
-insert into inventar(sifra,proizvod,kolicina,dostupnost)
-values (1,'2','250','dostupno');
+insert into inventar(proizvod,kolicina,dostupnost)
+values ('2','250','dostupno');
 
-insert into inventar(sifra,proizvod,kolicina,dostupnost)
-values (2,'3','100','nedostupno');
+insert into inventar(proizvod,kolicina,dostupnost)
+values ('3','100','nedostupno');
 
-insert into inventar(sifra,proizvod,kolicina,dostupnost)
-values (3,'1','50','dostupno');
+insert into inventar(proizvod,kolicina,dostupnost)
+values ('1','50','dostupno');
 
 SET IDENTITY_INSERT inventar OFF
 
@@ -93,14 +93,14 @@ SET IDENTITY_INSERT inventar OFF
 --TABLICA KOSARICA
 SET IDENTITY_INSERT kosarica ON
 
-insert into kosarica(sifra,kolicina,kupac,proizvod)
-values (1,'20','1','3');
+insert into kosarica(kolicina,kupac,proizvod)
+values ('20','1','3');
 
-insert into kosarica(sifra,kolicina,kupac,proizvod)
-values (2,'1','2','2');
+insert into kosarica(kolicina,kupac,proizvod)
+values ('1','2','2');
 
-insert into kosarica(sifra,kolicina,kupac,proizvod)
-values (3,'2','3','1');
+insert into kosarica(kolicina,kupac,proizvod)
+values ('2','3','1');
 
 SET IDENTITY_INSERT kosarica OFF
 
