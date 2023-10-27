74c21747754f7d3bd9f003906020191e43ef44e1
Ana <131706932+Patrikgomercic1@users.noreply.github.com>
2023-10-27 10:07:36 +0200

    Pogon

--- OnlineTrgovinaWebAPI/OnlineTrgovina/Controllers/AutorizacijaController.cs
+++ OnlineTrgovinaWebAPI/OnlineTrgovina/Controllers/AutorizacijaController.cs
@@ -24,47 +24,7 @@ namespace EdunovaAPI.Controllers
 
 
 
-        [HttpPost("token")]
-        public IActionResult GenerirajToken(Operater operater)
-        {
-
-         
-            var operBaza = _context.Operater
-                   .Where(p => p.Email!.Equals(operater.Email))
-                   .FirstOrDefault();
-
-            if (operBaza == null)
-            {
-                // Šaljem Status403Forbidden jer forntend hvata sve 401 i baca na login pa nikada ne dobijem poruku da
-                // nije dobro korisničko ime ili lozinka
-                return StatusCode(StatusCodes.Status403Forbidden, "Niste autorizirani, ne mogu naći operatera");
-            }
-
-
        
-            if(!BCrypt.Net.BCrypt.Verify(operater.Lozinka, operBaza.Lozinka))
-            {
-                return StatusCode(StatusCodes.Status403Forbidden, "Niste autorizirani, lozinka ne odgovara");
-            }
-
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.UTF8.GetBytes("MojKljucKojijeJakoTajan");
-
-            
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Expires = DateTime.UtcNow.Add(TimeSpan.FromHours(8)),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
-            };
-
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            var jwt = tokenHandler.WriteToken(token);
-
-
-            return Ok(jwt);
-
-        }
 	}
 }
 
